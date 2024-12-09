4c7784c0274b8b2aa5c872b9c104081ea3c4ddcc
pktolstov <pktolstov@gmail.com>
2024-12-09 15:54:47 +0300

    Improved red color click icon and optimized initAddLike function

--- script.js
+++ script.js
@@ -13,18 +13,25 @@ const initAddLike = () => {
     for (const comment of findAllComments) {
         comment.addEventListener('click', () => {
             const commentIndex = comment.dataset.index;
-            if (comments[commentIndex].islike === false) {
-                comments[commentIndex].numberOfLikes += 1;
-                comments[commentIndex].islike = true;
-                comment.classList.add("svg-class")
-                console.log(comment.firstElementChild.firstElementChild.innerHTML = '<image href="img/coolicon_red.svg" height="20px" width="20px" />')
-                comment.firstElementChild.firstElementChild.innerHTML = '<image href="img/coolicon_red.svg" height="20px" width="20px" />';
-                renderComments();
-            } else {
-                comments[commentIndex].numberOfLikes -= 1;
-                comments[commentIndex].islike = false;
-                renderComments();
-            };
+            let islike = true;
+            let countLike = 1;
+            if (comments[commentIndex].islike) {
+                islike = false
+                countLike = -1
+            }
+            comments[commentIndex].islike = islike;
+            comments[commentIndex].numberOfLikes += countLike;
+
+            //Old version for compareing
+            // if (comments[commentIndex].islike === false) {
+            //     comments[commentIndex].numberOfLikes += 1;
+            //     comments[commentIndex].islike = true;          
+            // } else {
+            //     comments[commentIndex].numberOfLikes -= 1;
+            //     comments[commentIndex].islike = false;
+            // };
+
+            renderComments();
 
         })
 
@@ -34,10 +41,11 @@ const initAddLike = () => {
 const renderComments = () => {
     const comentsHtml = comments.map((comment, index) => {
         //return `<li><span>${comment.text}</span></br><button  data-index="${index}" class="like">Нравится комментарий (${comment.numberOfLikes})</button></li>`
+        //<img src=`${comment.islike ? 'like.svg' : 'dislike.svg'}`>
         return `<li><span>${comment.text}</span></br>
         <div data-index="${index}"class="like">
             <svg data-svg="${index}-svg" width="20px" height="20px" xmlns="http://www.w3.org/2000/svg">
-            <image href="img/coolicon.svg" height="20px" width="20px" />
+            <image href="${comment.islike ? 'img/dislike.svg' : 'img/like.svg'}" height="20px" width="20px" />
             </svg><span>${comment.numberOfLikes}</span>
             </div></li>`
     }).join("");
