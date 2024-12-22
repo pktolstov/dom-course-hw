ed95826d4f0820671670753b5f3fdc0d26627f9e
pktolstov <pktolstov@gmail.com>
2024-12-22 23:25:02 +0300

    Improved Eslint config beacause off double-quotes errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,16 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import config from "eslint-config-prettier";
-import plugin from "eslint-plugin-prettier/recommended";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import config from 'eslint-config-prettier'
+import plugin from 'eslint-plugin-prettier/recommended'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  config,
   plugin,
-];
+  config,
+]
+
+"rules": {
+  "prettier/prettier": ["error", { "singleQuote": true }]
+}
